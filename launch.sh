83a9bc062712ece162fa7753182e6d35f445f307
erichugy <eric.huang5@mail.mcgill.ca>
2024-11-19 02:53:19 -0500

    updated launch because created own env

--- launch.sh
+++ launch.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #SBATCH --nodes 1
-#SBATCH --gres=gpu:1 # request a GPU
+#SBATCH --gres=gpu:1 # request a GPU|
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
 #SBATCH --time=01:00:00
 #SBATCH --output=%N-%j.out
 #SBATCH --account=fall2024-comp551
-#SBATCH -e %N-%j.err # STDERR
 
 module load cuda/cuda-12.6
 module load miniconda/miniconda-fall2024
+source ~/.bashrc
+conda activate comp551 
 
 python code/run.py
-
 

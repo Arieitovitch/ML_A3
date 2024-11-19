d7f260aa730b898cb9508acf3c4cdbff881c28df
erichugy <eric.huang5@mail.mcgill.ca>
2024-11-18 19:57:54 -0500

    Updated launch.sh

--- setup_conda.sh
+++ setup_conda.sh
@@ -4,14 +4,9 @@
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
-#SBATCH --time=01:00:00
+#SBATCH --time=00:10:00
 #SBATCH --output=%N-%j.out
 #SBATCH --account=fall2024-comp551
-#SBATCH -e %N-%j.err # STDERR
 
-module load cuda/cuda-12.6
 module load miniconda/miniconda-fall2024
-
-python code/run.py
-
-
+conda init bash
